d28607579d22069ba94be98da68a9c2adc6152b8
balajiyatheysh <balajiyatheysh87@gmail.com>
2024-02-11 21:50:49 +0530

    11-2-2024

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -6,24 +6,31 @@ import { API_OPTIONS } from "../utils/constants";
 import { addGptMovieResult } from "../utils/gptSlice";
 
 const GptSearchBar = () => {
+
   const dispatch = useDispatch();
+
   const langKey = useSelector((store) => store.config.lang);
+
   const searchText = useRef(null);
 
   // search movie in TMDB
   const searchMovieTMDB = async (movie) => {
+
     const data = await fetch(
       "https://api.themoviedb.org/3/search/movie?query=" +
         movie +
         "&include_adult=false&language=en-US&page=1",
       API_OPTIONS
     );
+
     const json = await data.json();
 
     return json.results;
+
   };
 
   const handleGptSearchClick = async () => {
+
     console.log(searchText.current.value);
     // Make an API call to GPT API and get Movie Results
 
@@ -60,9 +67,11 @@ const GptSearchBar = () => {
     dispatch(
       addGptMovieResult({ movieNames: gptMovies, movieResults: tmdbResults })
     );
+
   };
 
   return (
+
     <div className="pt-[35%] md:pt-[10%] flex justify-center">
       <form
         className="w-full md:w-1/2 bg-black grid grid-cols-12"
@@ -82,6 +91,8 @@ const GptSearchBar = () => {
         </button>
       </form>
     </div>
+    
   );
 };
+
 export default GptSearchBar;
