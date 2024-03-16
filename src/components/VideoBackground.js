47f5c9ffe8df8a9ff65527014987321dfb6c0487
balajiyatheysh <balajiyatheysh87@gmail.com>
2024-03-16 17:29:13 +0530

    16-3-2024

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -8,6 +8,7 @@ const VideoBackground = ({ movieId }) => {
 
   return (
     <div className=" w-screen">
+
       <iframe
         className="w-screen aspect-video"
         src={
@@ -18,6 +19,7 @@ const VideoBackground = ({ movieId }) => {
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
+      
     </div>
   )
 }
